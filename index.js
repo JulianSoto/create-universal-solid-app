8e84b935284d68c5d16ecad1697a8c8b3569d65e
julian <juliansotoponce@gmail.com>
2024-06-10 12:36:52 -0700

    fix: use correct __dirname and cwd

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const { argv } = require('node:process');
+const { argv, cwd } = require('node:process');
 const sanitize = require('sanitize-filename');
 
 const projectName = sanitize(argv[2]);
-const appRoot = __dirname + '/' + projectName;
+const appRoot = cwd() + '/' + projectName;
 
 fs.mkdirSync(appRoot);
 console.info('Initializing project...');
-fs.cpSync('./templates/ts', appRoot, { recursive: true });
+fs.cpSync(__dirname + '/templates/ts', appRoot, { recursive: true });
 console.info(`Project initialized! Run 'cd ${projectName}' then 'npm install'`);
